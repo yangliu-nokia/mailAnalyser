0f32dd28cf33b7bade5fe52f3c91a853a9421e12
yangliu-nokia <yang.7.liu@nokia.com>
2017-11-30 14:34:22 +0100

    Initial Commit

---
+++ MailAnalyser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MailAnalyser
+//
+//  Created by Liu, Yang 7. (Nokia - FR/Nozay) on 30/11/2017.
+//  Copyright © 2017 Liu, Yang 7. (Nokia - FR/Nozay). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
